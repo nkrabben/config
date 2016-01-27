4fff9f8f00b1fe8b58295397ba8716efa617e086
nkrabben <n@nkrabben.com>
2016-01-27 17:20:14 -0500

    add linters to atom

--- installAtomConfig.sh
+++ installAtomConfig.sh
@@ -1,7 +1,12 @@
 atomlibs=(
 # Code lint
+  linter
   linter-alex
   linter-pylint
+  linter-lintr
+  linter-tidy
+  linter-js-yaml
+  linter-csslint
 )
 echo "### installing atom extensions..."
 pip install ${atomlibs[@]}
