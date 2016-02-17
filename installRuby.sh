34ccd7ecb0e23873a25f871d574cf1e61fb72c32
nkrabben <n@nkrabben.com>
2016-02-17 14:09:45 -0500

    fix rbenv confusion

--- installRuby.sh
+++ installRuby.sh
@@ -5,6 +5,7 @@ brew install rbenv ruby-build
 
 # Add rbenv to bash so that it loads every time you open a terminal
 echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.zshrc
+eval "$(rbenv init -)"
 
 # Install Ruby
 rbenv install 2.2.3
